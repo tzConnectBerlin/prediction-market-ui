db8a314dc3420005cb8cf7beb18790353204228c
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2020-12-03 16:09:42 +0100

    feat(wallet): add basic thanos and beacon integration

---
+++ src/interfaces/wallet.ts
@@ -0,0 +1,16 @@
+import { ThanosDAppNetwork, ThanosWallet } from '@thanos-wallet/dapp';
+import { BeaconWallet } from '@taquito/beacon-wallet';
+
+export type WalletType = 'Thanos' | 'Beacon';
+export type NetworkType = 'mainnet' | 'delphinet' | 'carthagenet' | 'sandbox';
+
+export type ThanosNetworkType = Exclude<ThanosDAppNetwork, { rpc: string; name: string }>;
+
+export type WalletInstanceType = ThanosWallet | BeaconWallet;
+
+export interface WalletInterface {
+  type: WalletType;
+  pkh?: string;
+  wallet: WalletInstanceType;
+  network?: string;
+}
