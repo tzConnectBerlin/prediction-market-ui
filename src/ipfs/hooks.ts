b88307171d1d60c1dba5cd53ec24b855d9f9dac1
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2020-12-08 15:37:51 +0100

    fix(ipfs): fix the ipfs setup

--- src/ipfs/hooks.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import IPFSContext from './IpfsContext';
-
-export const useIpfs = (): any => {
-  const { ipfs } = useContext(IPFSContext);
-  return ipfs;
-};
--- src/ipfs/ipfs.ts
+++ src/ipfs/ipfs.ts
@@ -1,7 +1,16 @@
-import IPFS from 'ipfs-core';
+import ipfsClient from 'ipfs-http-client';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const getIpfsInstance = async () => {
-  const ipfs = await IPFS.create();
-  return ipfs;
+const FETCH_IPFS_PROVIDER = 'https://ipfs.io/ipfs/';
+const IPFS_PROVIDER = 'http://127.0.0.1:5001';
+
+export const fetchIPFSData = async <T>(cid: string, provider = FETCH_IPFS_PROVIDER): Promise<T> => {
+  const data = await fetch(`${provider}${cid}`);
+  const result = await data.json();
+  return result;
+};
+
+export const addIPFSData = async <T>(data: T, provider = IPFS_PROVIDER): Promise<string> => {
+  const ipfs = ipfsClient({ url: provider });
+  const response = await ipfs.add(JSON.stringify(data) as any);
+  return response.path;
 };
