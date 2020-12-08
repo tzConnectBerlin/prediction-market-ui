5a843e2adfb90858d91bc6ec5a153e1cff0aa934
Shubhendu Shekhar <9253059+shekhar-shubhendu@users.noreply.github.com>
2020-12-08 12:13:25 +0100

    feat(wallet): add wallet context, hook and basic connection flow (#26)
    * feat(wallet): add wallet context, hook and basic connection flow
    
    * fix(beacon): fix typo

--- src/wallet/manager.ts
+++
@@ -1,7 +0,0 @@
-import { WalletType, NetworkType } from '../interfaces/wallet';
-
-const toWalletNetworkType = (network: NetworkType, walletType: WalletType): string => {
-  if (walletType === 'Beacon' && network === 'sandbox') return 'CUSTOM';
-  if (walletType === 'Beacon') return network.toUpperCase();
-  return network;
-};
