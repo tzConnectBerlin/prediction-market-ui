01e7e9f28883b918de535b0cd2a4da6db0b825bc
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2020-12-21 12:34:13 +0100

    feat(tab-title): set page title to the tab and add langugae awareness

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,13 +3,16 @@ import detector from 'i18next-browser-languagedetector';
 import backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
+export const DEFAULT_LANGUAGE = 'en';
+export const FALLBACK_LANGUAGE = 'en';
+
 i18n
   .use(detector)
   .use(backend)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    lng: 'en',
-    fallbackLng: 'en', // use en if detected lng is not available
+    lng: DEFAULT_LANGUAGE,
+    fallbackLng: FALLBACK_LANGUAGE, // use en if detected lng is not available
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
