b95790cb1638df584f9e4427e017ef81a17107fb
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-01-04 16:35:16 +0100

    feat(api): add bcd api and first calls

---
+++ src/interfaces/bcd.ts
@@ -0,0 +1,64 @@
+export interface SimilarContract {
+  added?: number;
+  address: string;
+  alias?: string;
+  annotations?: string[];
+  balance?: number;
+  count?: number;
+  delegate?: string;
+  delegate_alias?: string;
+  entrypoints?: string[];
+  fail_strings?: string[];
+  found_by?: string;
+  hardcoded?: string[];
+  hash: string;
+  id?: string;
+  language?: string;
+  last_action?: string;
+  level?: number;
+  manager?: string;
+  migrations_count?: number;
+  network?: string;
+  project_id?: string;
+  removed?: number;
+  slug?: string;
+  subscription?: Subscription;
+  tags?: string[];
+  timestamp?: string;
+  tokens?: TokenBalance[];
+  total_subscribed?: number;
+  total_withdrawn?: number;
+  tx_count?: number;
+  verification_source?: string;
+  verified?: boolean;
+}
+
+export interface Subscription {
+  address?: string;
+  alias?: string;
+  network?: string;
+  sentry_dsn?: string;
+  sentry_enabled?: boolean;
+  subscribed_at?: string;
+  watch_calls?: boolean;
+  watch_deployments?: boolean;
+  watch_errors?: boolean;
+  watch_mempool?: boolean;
+  watch_migrations?: boolean;
+  watch_same?: boolean;
+  watch_similar?: boolean;
+}
+
+export interface TokenBalance {
+  balance?: number;
+  contract?: string;
+  decimals?: number;
+  name?: string;
+  symbol?: string;
+  token_id?: number;
+}
+
+export interface SimilarContractResponse {
+  contracts?: SimilarContract[];
+  count?: number;
+}
