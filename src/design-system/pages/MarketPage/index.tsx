f62ac7fcc1bc492f9df1331e9cf2791fb78434bb
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-01-26 15:25:19 +0100

    feat(market-filter): add basic market filter

--- src/design-system/pages/MarketPage/index.tsx
+++ src/design-system/pages/MarketPage/index.tsx
@@ -1,4 +1,11 @@
-import { Grid, Paper, CircularProgress } from '@material-ui/core';
+import {
+  Grid,
+  Paper,
+  CircularProgress,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+} from '@material-ui/core';
 import { AxiosError } from 'axios';
 import { useQuery } from 'react-query';
 import React from 'react';
@@ -21,11 +28,54 @@ interface MarketList {
   marketClosed: { [key: string]: MarketCardProps };
 }
 
+interface SelectionBoxState {
+  auctions: boolean;
+  allMarkets: boolean;
+  openMarkets: boolean;
+  closedMarkets: boolean;
+}
+
 export const MarketPageComponent: React.FC<MarketPageProps> = ({ t }) => {
   const history = useHistory();
   const marketHashes = {
     auction: new Array<string>(),
     other: new Array<string>(),
+  };
+  const [state, setState] = React.useState<SelectionBoxState>({
+    auctions: true,
+    allMarkets: false,
+    openMarkets: true,
+    closedMarkets: false,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newState: SelectionBoxState = state;
+    if (event.target.name === 'allMarkets') {
+      newState = {
+        ...newState,
+        closedMarkets: event.target.checked,
+        openMarkets: event.target.checked,
+        allMarkets: event.target.checked,
+      };
+    } else {
+      newState = {
+        ...newState,
+        [event.target.name]: event.target.checked,
+      };
+    }
+    if (newState.openMarkets && newState.closedMarkets) {
+      newState = {
+        ...newState,
+        allMarkets: true,
+      };
+    }
+    if (!newState.openMarkets || !newState.closedMarkets) {
+      newState = {
+        ...newState,
+        allMarkets: false,
+      };
+    }
+    setState(newState);
   };
   const AuctionInfo: React.FC<AuctionData> = ({ yes, participants }) => (
     <>
@@ -114,11 +164,50 @@ export const MarketPageComponent: React.FC<MarketPageProps> = ({ t }) => {
   return (
     <MainPage title={title ? t(`${title}`) : undefined}>
       {isLoading && <CircularProgress />}
-      {marketList && Object.entries(marketList.auctionOpen).length > 0 && (
+      <FormGroup row>
+        <FormControlLabel
+          control={<Checkbox checked={state.auctions} onChange={handleChange} name="auctions" />}
+          label={t('auctionOpen')}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={state.allMarkets}
+              onChange={handleChange}
+              name="allMarkets"
+              color="primary"
+            />
+          }
+          label={t('allMarkets')}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={state.openMarkets}
+              onChange={handleChange}
+              name="openMarkets"
+              color="primary"
+            />
+          }
+          label={t('openMarket')}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={state.closedMarkets}
+              onChange={handleChange}
+              name="closedMarkets"
+              color="primary"
+            />
+          }
+          label={t('closedMarket')}
+        />
+      </FormGroup>
+      {marketList && state.auctions && Object.entries(marketList.auctionOpen).length > 0 && (
         <Paper elevation={0}>
           <>
             <Typography component="span" size="h4">
-              {t('auctionOpenSection')}
+              {t('auctionOpen')}
             </Typography>
             <Grid container spacing={1}>
               {Object.entries(marketList.auctionOpen).map(([hash, item]) => (
@@ -130,11 +219,11 @@ export const MarketPageComponent: React.FC<MarketPageProps> = ({ t }) => {
           </>
         </Paper>
       )}
-      {marketList && Object.entries(marketList.marketOpen).length > 0 && (
+      {marketList && state.openMarkets && Object.entries(marketList.marketOpen).length > 0 && (
         <Paper elevation={0}>
           <>
             <Typography component="span" size="h4">
-              {t('openMarketSection')}
+              {t('openMarket')}
             </Typography>
             <Grid container spacing={1}>
               {Object.entries(marketList.marketOpen).map(([hash, item]) => (
@@ -146,11 +235,11 @@ export const MarketPageComponent: React.FC<MarketPageProps> = ({ t }) => {
           </>
         </Paper>
       )}
-      {marketList && Object.entries(marketList.marketClosed).length > 0 && (
+      {marketList && state.closedMarkets && Object.entries(marketList.marketClosed).length > 0 && (
         <Paper elevation={0}>
           <>
             <Typography component="span" size="h4">
-              {t('closedMarketSection')}
+              {t('closedMarket')}
             </Typography>
             <Grid container spacing={1}>
               {Object.entries(marketList.marketClosed).map(([hash, item]) => (
