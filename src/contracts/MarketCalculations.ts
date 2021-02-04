650130bc56d505fcbe66c5d68e0c4828914756df
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-02-04 12:58:46 +0100

    feat(market-calc): add market related calculations

---
+++ src/contracts/MarketCalculations.ts
@@ -0,0 +1,68 @@
+const MARKET_FEE = 0.0003;
+
+interface ClosePositionReturn {
+  aToSwap: number;
+  aLeft: number;
+  bReceived: number;
+}
+
+interface ClosePositionBothReturn {
+  aToSwap: number;
+  aLeft: number;
+  bHeld: number;
+}
+
+export const fixedInSwap = (aPool: number, bPool: number, fixedAIn: number): number => {
+  const k = aPool * bPool;
+  return (k / (aPool + (1 - MARKET_FEE) * fixedAIn) - bPool) * -1;
+};
+
+export const optimalSwap = (aPool: number, bPool: number, aHoldings: number): number => {
+  const a = 1 - MARKET_FEE;
+  const b = aPool + bPool * (1 - MARKET_FEE) - aHoldings * (1 - MARKET_FEE);
+  const c = -1 * aPool * aHoldings;
+  const deltaA = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+  return deltaA;
+};
+
+export const optimalSwapBoth = (
+  aPool: number,
+  bPool: number,
+  aHoldings: number,
+  bHoldings: number,
+): number => {
+  const a = 1 - MARKET_FEE;
+  const b =
+    aPool + bPool * (1 - MARKET_FEE) - aHoldings * (1 - MARKET_FEE) + bHoldings * (1 - MARKET_FEE);
+  const c = aPool * (bHoldings - aHoldings);
+  const deltaA = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+  return deltaA;
+};
+
+export const closePosition = (
+  aPool: number,
+  bPool: number,
+  aHoldings: number,
+): ClosePositionReturn => {
+  const aToSwap = optimalSwap(aPool, bPool, aHoldings);
+  const aLeft = aHoldings - aToSwap;
+  const bReceived = fixedInSwap(aPool, bPool, aToSwap);
+  return {
+    bReceived,
+    aLeft,
+    aToSwap,
+  };
+};
+
+export const closePositionBoth = (
+  aPool: number,
+  bPool: number,
+  aHoldings: number,
+  bHoldings: number,
+): ClosePositionBothReturn => {
+  const aToSwap = optimalSwapBoth(aPool, bPool, aHoldings, bHoldings);
+  const aLeft = aHoldings - aToSwap;
+  const bReceived = fixedInSwap(aPool, bPool, aToSwap);
+  const bHeld = bHoldings + bReceived;
+  return { aToSwap, aLeft, bHeld };
+};
