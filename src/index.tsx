7a50d598c6225e934b00e859852b46525b72898e
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-04-12 15:36:54 +0200

    feat(custom-input-label): add custom input label component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Global, css } from '@emotion/react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import './index.css';
+import '@fontsource/roboto';
 import './i18n';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -10,6 +11,16 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+            font-family: 'Roboto', sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+        `}
+      />
       <App />
     </Provider>
   </React.StrictMode>,
