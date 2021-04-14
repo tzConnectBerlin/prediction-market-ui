bee671b219bb52815689b02b3c47ef7f39559f8b
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-04-14 20:45:27 +0200

    fix(custom-input-label): update tests

--- src/design-system/atoms/CustomInputLabel/CustomInputLabel.test.tsx
+++ src/design-system/atoms/CustomInputLabel/CustomInputLabel.test.tsx
@@ -1,7 +1,8 @@
 import renderer from 'react-test-renderer';
-import { render, waitFor } from '@testing-library/react';
-import { theme } from '../../../theme';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from '@material-ui/core';
 import { CustomInputLabelProps, CustomInputLabel } from './CustomInputLabel';
+import { theme } from '../../../theme';
 
 const onlyRequiredProps: CustomInputLabelProps = {
   label: 'Create a question',
@@ -10,35 +11,34 @@ const onlyRequiredProps: CustomInputLabelProps = {
 const defaultProps: CustomInputLabelProps = {
   ...onlyRequiredProps,
   marginTop: '1%',
-  theme,
 };
+
+const WrappedComponent: React.FC = (args: any) => (
+  <ThemeProvider theme={theme}>
+    <CustomInputLabel {...args} />
+  </ThemeProvider>
+);
 
 describe('Snapshot testing CustomInputLabel Component', () => {
   it('renders correctly with default props', () => {
-    const inputLabel = renderer.create(<CustomInputLabel {...defaultProps} />).toJSON();
+    const inputLabel = renderer.create(<WrappedComponent {...defaultProps} />).toJSON();
     expect(inputLabel).toMatchSnapshot();
   });
 
   it('renders correctly when theme not provided', () => {
-    const inputLabel = renderer.create(<CustomInputLabel {...onlyRequiredProps} />).toJSON();
+    const inputLabel = renderer.create(<WrappedComponent {...onlyRequiredProps} />).toJSON();
     expect(inputLabel).toMatchSnapshot();
   });
 });
 
 describe('Element testing CustomInputLabel Component', () => {
   it('render correctly CustomInputLabel with default props', async () => {
-    const { findByText } = render(<CustomInputLabel {...defaultProps} />);
-
-    waitFor(() => {
-      expect(findByText(/Create a question/i)).toBeInTheDocument();
-    });
+    const { getByText } = render(<WrappedComponent {...defaultProps} />);
+    expect(getByText(/Create a question/i)).toBeInTheDocument();
   });
 
   it('render correctly CustomInputLabel with only required props', async () => {
-    const { findByText } = render(<CustomInputLabel {...defaultProps} />);
-
-    waitFor(() => {
-      expect(findByText(/Create a question/i)).toBeInTheDocument();
-    });
+    const { getByText } = render(<WrappedComponent {...defaultProps} />);
+    expect(getByText(/Create a question/i)).toBeInTheDocument();
   });
 });
