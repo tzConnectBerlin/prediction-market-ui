20da18b376b384248fef640c542f64e52da711f4
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-04-26 11:10:55 +0200

    fix(styles): update styles to fix date/time picker

--- src/assets/styles/style.ts
+++ src/assets/styles/style.ts
@@ -29,8 +29,11 @@ export const GlobalStyle = (theme: Theme): SerializedStyles => css`
       background-color: ${theme.palette.primary.main};
       color: ${theme.palette.buttonText.disabled};
     }
-    .MuiFormHelperText-root{
-      color: ${theme.palette.warning.main}
+    .MuiFormHelperText-root {
+      color: ${theme.palette.text.secondary};
+      &:not(.extra-help-message) {
+        color: ${theme.palette.warning.main};
+      }
     }
     .MuiInput-root.MuiInputBase-formControl {
       &:before,
@@ -39,28 +42,21 @@ export const GlobalStyle = (theme: Theme): SerializedStyles => css`
       &:after {
         border-bottom: none;
       }
-      input,
-      textarea {
-        background-color: ${theme.palette.grey[400]};
-        border: solid 1px ${theme.palette.grey[600]};
-        border-radius: ${theme.spacing(1 / 4)};
-        padding-left: ${theme.spacing(1 / 2)};
-        padding-right: ${theme.spacing(1 / 2)};
-  
-        &:not(.Mui-disabled){
-          &:hover {
-            background-color: ${theme.palette.grey[500]};
-          }
-          &:focus {
-            border-color: ${theme.palette.primary.main};
-          }
+      background-color: ${theme.palette.grey[400]};
+      border: solid 1px ${theme.palette.grey[600]};
+      border-radius: ${theme.spacing(1 / 4)};
+      padding-left: ${theme.spacing(1 / 2)};
+      padding-right: ${theme.spacing(1 / 2)};
+      &:not(.Mui-disabled) {
+        &:hover {
+          background-color: ${theme.palette.grey[500]};
+        }
+        &:focus {
+          border-color: ${theme.palette.primary.main};
         }
       }
-      &.Mui-error{
-        input,
-        textarea {
-          border-color: ${theme.palette.warning.main}
-        }
+      &.Mui-error {
+        border-color: ${theme.palette.warning.main};
       }
       .MuiSelect-select {
         padding-left: ${theme.spacing(1 / 2)};
@@ -70,7 +66,7 @@ export const GlobalStyle = (theme: Theme): SerializedStyles => css`
       }
     }
   }
-  .MuiInputLabel-formControl.MuiInputLabel-root{
+  .MuiInputLabel-formControl.MuiInputLabel-root {
     font-weight: bold;
     color: ${theme.palette.primary.main};
     position: relative;
@@ -125,4 +121,5 @@ export const GlobalStyle = (theme: Theme): SerializedStyles => css`
         margin-left: -0.75em;
       }
     }
+  }
 `;
