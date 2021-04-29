e05bfb04b31a22dbed2483710fdf6a856d9b4b37
cpooley <cynthia.pooley@gmail.com>
2021-04-29 21:30:50 +0200

    remove extra grid item

--- src/design-system/molecules/Footer/Footer.tsx
+++ src/design-system/molecules/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Box, Button, Grid, Link, Toolbar } from '@material-ui/core';
+import { AppBar, Grid, Toolbar } from '@material-ui/core';
 import { Typography } from '../../atoms/Typography';
 import { ArrowRtIcon } from '../../atoms/ArrowRtIcon';
 
@@ -27,8 +27,8 @@ export const Footer: React.FC<FooterProps> = ({
       sx={{ display: 'flex', marginTop: 2, width: '100%' }}
     >
       <Toolbar>
-        <Grid container marginY={1.5} marginX={1.5} justifyContent="center">
-          <Grid container direction="column" xs={12} sm={8} md={6}>
+        <Grid container marginY={1.5} marginX={2.5}>
+          <Grid container direction="column" justifyContent="flex-starts" xs={12} md={8}>
             <Grid item>
               <Typography
                 size="1.375em"
@@ -47,9 +47,16 @@ export const Footer: React.FC<FooterProps> = ({
               </Typography>
             </Grid>
           </Grid>
-          <Grid item xs={12} sm={1} md={2} lg={3} />
-          <Grid container xs={12} sm={3} md={4} lg={3} justifyContent="center">
-            <Grid container item xs={12} sm={12} md={6} justifyContent="center">
+          <Grid container xs={12} md={4} justifyContent="flex-end">
+            <Grid
+              container
+              item
+              xs={12}
+              md={6}
+              sx={{
+                justifyContent: { xs: 'center', md: 'flex-end' },
+              }}
+            >
               <Typography
                 onClick={handleSecondaryAction}
                 color="primary"
@@ -58,7 +65,15 @@ export const Footer: React.FC<FooterProps> = ({
                 {footerWorks}
               </Typography>
             </Grid>
-            <Grid container item xs={12} sm={12} md={6} justifyContent="center">
+            <Grid
+              container
+              item
+              xs={12}
+              md={6}
+              sx={{
+                justifyContent: { xs: 'center', md: 'flex-end' },
+              }}
+            >
               <Typography
                 onClick={handleSecondaryAction}
                 color="primary"
