e1cc7312b1350f343d8b707b0ba406bde77fbc9c
Shubhendu Shekhar <9253059+shekhar-shubhendu@users.noreply.github.com>
2021-05-17 16:05:37 +0200

    feat(market-detail-card): add market detail card component (#298)

---
+++ src/design-system/molecules/MarketDetailCard/MarketDetailCard.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { MarketDetailCard, MarketDetailCardProps } from './MarketDetailCard';
+
+export default {
+  title: 'Molecule/MarketDetailCard',
+  component: MarketDetailCard,
+  argTypes: {},
+} as Meta;
+
+const descriptionArgs = {
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'.repeat(20),
+  expandActionText: 'Read more',
+  shrinkActionText: 'Read less',
+};
+
+const Template: Story<MarketDetailCardProps> = (args) => <MarketDetailCard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'About Market',
+  items: [
+    {
+      title: 'Description',
+      item: descriptionArgs,
+    },
+    {
+      title: 'Ticker',
+      item: '$TZBDN',
+    },
+  ],
+};
