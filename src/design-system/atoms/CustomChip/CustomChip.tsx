516d12aa4a997d06efa60068d1b66fe8697b9107
shmodarresi <sh.modarresy@gmail.com>
2021-05-26 17:30:39 +0200

    style(customchip): add style to customChip

--- src/design-system/atoms/CustomChip/CustomChip.tsx
+++ src/design-system/atoms/CustomChip/CustomChip.tsx
@@ -1,11 +1,41 @@
-import { Chip, ChipProps } from '@material-ui/core';
 import React from 'react';
+import styled from '@emotion/styled';
+import { Chip, ChipProps } from '@material-ui/core';
+import { lightTheme as theme } from '../../../theme';
 
-export const CustomChip: React.FC<ChipProps> = ({
-  size = 'small',
+const StyledChip = styled(Chip)`
+  &.xs {
+    font-size: ${theme.spacing(3 / 2)};
+    height: ${theme.spacing(5 / 2)};
+  }
+
+  &.sm {
+    font-size: ${theme.spacing(3 / 2)};
+    height: ${theme.spacing(3)};
+  }
+
+  &.md {
+    font-size: ${theme.spacing(2)};
+    height: ${theme.spacing(10 / 3)};
+    padding: 0 ${theme.spacing(3 / 2)};
+  }
+
+  &.lg {
+    font-size: ${theme.spacing(5 / 2)};
+    height: ${theme.spacing(4)};
+    padding: 0 ${theme.spacing(2)};
+  }
+`;
+
+export interface CustomChipProps extends ChipProps {
+  chipSize: 'xs' | 'sm' | 'md' | 'lg';
+}
+
+export const CustomChip: React.FC<CustomChipProps> = ({
+  chipSize = 'xs',
   color = 'secondary',
   onDelete = undefined,
   ...rest
 }) => {
-  return <Chip size={size} color={color} {...rest} />;
+  return <StyledChip color={color} className={chipSize} {...rest} />;
 };
