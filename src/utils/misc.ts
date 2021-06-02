37ea7cdc6f16ed1a81de8019ae7a82310a05e55c
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-06-02 14:54:55 +0200

    feat(timestamp): show time too

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -6,7 +6,7 @@ import { DATETIME_FORMAT } from './globals';
 export const getMarketStateLabel = (
   market: Market,
   t: TFunction,
-  timeStampFormat = DATETIME_FORMAT.SHORT_FORMAT,
+  timeStampFormat = DATETIME_FORMAT.MEDIUM_FORMAT,
 ): string => {
   if (market.state === MarketStateType.auctionRunning) {
     return format(new Date(market.auctionEndDate), timeStampFormat);
