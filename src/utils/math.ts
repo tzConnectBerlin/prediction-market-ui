e0832c73399d17795298030e66c3fe37d23f6f7f
Shubhendu Shekhar <9253059+shekhar-shubhendu@users.noreply.github.com>
2021-06-09 13:50:39 +0200

    feat: update time picker (#344)
    * feat(time-picker): add new time picker
    
    * fix: add links to footer
    
    * fix: update snapshots

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,4 +1,4 @@
-export const multiplyUp = (value: number, factor = 64): number => value * 2 ** factor;
+export const multiplyUp = (value: number, factor = 64): number => Math.floor(value * 2 ** factor);
 export const divideDown = (value: number, factor = 64): number => value / 2 ** factor;
 export const roundToTwo = (num: number): number => Math.round((num + Number.EPSILON) * 100) / 100;
 export const tokenMultiplyUp = (value: number, factor = 1e6): number => value * factor;
