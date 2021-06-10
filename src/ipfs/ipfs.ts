063d33d79f671e3210111ae21f5ad238ab05a919
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-06-10 09:16:54 +0200

    fix: remove repeated code

--- src/ipfs/ipfs.ts
+++ src/ipfs/ipfs.ts
@@ -3,6 +3,10 @@ import localForage from 'localforage';
 import { logError } from '../logger/logger';
 import { IPFS_API, IPFS_PORT } from '../utils/globals';
 
+const axiosIpfs = axios.create({
+  baseURL: `${IPFS_API}:${IPFS_PORT}/api/v0/`,
+});
+
 export const fetchIPFSData = async <T>(cid: string): Promise<T> => {
   try {
     const data = await localForage.getItem<T>(cid);
@@ -12,9 +16,7 @@ export const fetchIPFSData = async <T>(cid: string): Promise<T> => {
   } catch (error) {
     logError(error);
   }
-  const response = await axios.post<T>(
-    `${IPFS_API}:${IPFS_PORT}/api/v0/cat?encoding=json&arg=${cid}`,
-  );
+  const response = await axiosIpfs.post<T>(`cat?encoding=json&arg=${cid}`);
   await localForage.setItem(cid, response.data);
   return response.data;
 };
@@ -25,17 +27,12 @@ export const addIPFSData = async <T>(data: T): Promise<string> => {
   for (let i = 0; i < str.length; i += 1) {
     uint8Array[i] = str.charCodeAt(i);
   }
-  const blob = new Blob([uint8Array]);
   const formData = new FormData();
-  formData.set('file', blob);
-  const res = await axios.post(
-    `${IPFS_API}:${IPFS_PORT}/api/v0/add?stream-channels=true&progress=false&pin=true`,
-    formData,
-    {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
+  formData.set('file', new Blob([uint8Array]));
+  const res = await axiosIpfs.post(`add?stream-channels=true&progress=false&pin=true`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
     },
-  );
+  });
   return res.data.Hash;
 };
