f3eaa387d3e6ea1e2e273e70f4703dc90e39c094
cpooley <cynthia.pooley@gmail.com>
2021-08-02 12:05:00 +0200

    add grey icon and add to TwitterShare

--- src/design-system/atoms/TwitterShare/TwitterShare.tsx
+++ src/design-system/atoms/TwitterShare/TwitterShare.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Grid, Link, Paper, useTheme } from '@material-ui/core';
+import { Grid, useTheme } from '@material-ui/core';
 import { Typography } from '../Typography';
 
-export const TwitterShare = () => {
+export interface TwitterShareProps {
+  color?: string;
+}
+
+export const TwitterShare: React.FC<TwitterShareProps> = ({ color = 'blue' }) => {
   const theme = useTheme();
+  const twitterColor =
+    color === 'grey'
+      ? '/twitter-dark-grey-circle.svg'
+      : '/Twitter social icons - circle - blue.svg';
   return (
     <Grid
       container
@@ -19,8 +27,8 @@ export const TwitterShare = () => {
         </Typography>
       </Grid>
       <Grid item width={25} height={25} marginTop={1}>
-        <a href="https://twitter.com/intent/tweet?text=I%20did%20something!" data-size="large">
-          <img src="/Twitter social icons - circle - blue.svg" alt="twitter-share" />
+        <a href="https://twitter.com/intent/tweet?text=I%20participated%20in%20the%20TZConnect%20Prediction%20Market!">
+          <img src={twitterColor} alt="twitter-share" />
         </a>
       </Grid>
     </Grid>
