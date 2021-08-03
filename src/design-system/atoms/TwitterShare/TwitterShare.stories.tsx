ca00a61cf2a9ff6e57b013ca73e4206f94c3ef26
cpooley <cynthia.pooley@gmail.com>
2021-08-03 18:25:05 +0200

    fix twitter story

--- src/design-system/atoms/TwitterShare/TwitterShare.stories.tsx
+++ src/design-system/atoms/TwitterShare/TwitterShare.stories.tsx
@@ -12,11 +12,15 @@ const Template: Story<TwitterShareProps> = (args) => <TwitterShare {...args} />;
 export const BlueTwitter = Template.bind({});
 BlueTwitter.args = {
   color: 'blue',
+  urlHref: 'https://tzconnect.com/prediction-market/10/is-this-a-bear',
+  twitterText: 'text=Is%20this%20a%20bear%3F%20',
+};
+
+export const GreyTwitter = Template.bind({});
+GreyTwitter.args = {
+  color: 'grey',
   marketQuestion: 'will-i-fix-my-washing-machine-this-weekend',
   marketId: 77,
   urlHostname: 'tzconnect.predictionmarket.com',
   twitterText: 'text=I%20created%20a%20prediction%20market',
 };
-
-export const GreyTwitter = Template.bind({});
-GreyTwitter.args = { color: 'grey' };
