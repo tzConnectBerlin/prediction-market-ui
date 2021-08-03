4186edffe7a45c1b2ce47de61f489619a653777d
shekhar-shubhendu <shubh.shekhar1991@gmail.com>
2021-08-03 10:26:59 +0200

    fix(dropdown): add default value

--- src/design-system/organisms/Toolbar/Toolbar.tsx
+++ src/design-system/organisms/Toolbar/Toolbar.tsx
@@ -16,6 +16,8 @@ export interface ToolbarProps {
   onSortSelect?: DropDownProps['onSelect'];
   onSearchChange: SearchBoxProps['onChange'];
   onFilterSelect: SearchBoxProps['onSelect'];
+  defaultFilterValue?: DropDownProps['defaultValue'];
+  defaultSortValue?: DropDownProps['defaultValue'];
 }
 
 export const Toolbar: React.FC<ToolbarProps> = ({
@@ -24,6 +26,8 @@ export const Toolbar: React.FC<ToolbarProps> = ({
   onSearchChange,
   onFilterSelect,
   onSortSelect,
+  defaultFilterValue,
+  defaultSortValue,
 }) => {
   const { t } = useTranslation(['common']);
   const inputSizeXS = sortItems ? 8 : 12;
@@ -31,11 +35,21 @@ export const Toolbar: React.FC<ToolbarProps> = ({
   return (
     <StyledGrid container spacing={2} paddingX={2}>
       <Grid item xs={inputSizeXS} sm={inputSizeSM}>
-        <SearchBox onSelect={onFilterSelect} onChange={onSearchChange} filterItems={filterItems} />
+        <SearchBox
+          onSelect={onFilterSelect}
+          onChange={onSearchChange}
+          filterItems={filterItems}
+          defaultFilterValue={defaultFilterValue}
+        />
       </Grid>
       {sortItems && onSortSelect && (
         <Grid item xs={4} sm={2}>
-          <DropDown label={t('Sort By')} items={sortItems} onSelect={onSortSelect} />
+          <DropDown
+            label={t('Sort By')}
+            items={sortItems}
+            onSelect={onSortSelect}
+            defaultValue={defaultSortValue}
+          />
         </Grid>
       )}
     </StyledGrid>
