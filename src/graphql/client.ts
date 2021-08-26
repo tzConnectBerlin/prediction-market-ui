c90f177b61a4eeb2c9d8e367262cf121045e4933
shekhar-shubhendu <shubh.shekhar1991@gmail.com>
2021-08-26 12:29:30 +0200

    fix: base setup

---
+++ src/graphql/client.ts
@@ -0,0 +1,41 @@
+import { ApolloClient, InMemoryCache, split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { HttpLink } from '@apollo/client/link/http';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { persistCache, LocalForageWrapper } from 'apollo3-cache-persist';
+import localForage from 'localforage';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:5001/graphql', // use https for secure endpoint
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:5001/graphql', // use wss for a secure endpoint
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
+
+const cache = new InMemoryCache();
+const storage = new LocalForageWrapper(localForage);
+
+persistCache({
+  cache,
+  storage,
+  trigger: 'background',
+});
+
+export const graphqlClient = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
