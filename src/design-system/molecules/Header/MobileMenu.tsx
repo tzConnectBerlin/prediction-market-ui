c0b41f86f49fdf2f8e132b2d1c62e06728431692
Timothy Le Bon <tlebon@gmail.com>
2021-09-29 07:39:57 +0200

    refactor(menu): updating tests and refactoring menu structure

--- src/design-system/molecules/Header/MobileMenu.tsx
+++ src/design-system/molecules/Header/MobileMenu.tsx
@@ -1,40 +1,11 @@
 import * as React from 'react';
-import { Grid, Divider, IconButton, Drawer, Fade, Theme, useTheme } from '@material-ui/core';
-import { useTranslation } from 'react-i18next';
+import { Grid, IconButton, Drawer, Fade, Theme, useTheme } from '@material-ui/core';
 import styled from '@emotion/styled';
 import { AccountCircleRounded } from '@material-ui/icons';
-import { Address } from '../../atoms/Address/Address';
-import { SettingDialog } from '../SettingDialog';
-import { Typography } from '../../atoms/Typography';
-import { Loading } from '../../atoms/Loading';
-import { roundToTwo } from '../../../utils/math';
 import { CustomButton } from '../../atoms/Button';
 import { Identicon } from '../../atoms/Identicon';
 import { Links } from '../../../interfaces';
-
-const StyledGrid = styled(Grid)`
-  padding: 1rem;
-  .settings {
-    padding-top: 0;
-  }
-  .header-container {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-  }
-`;
-
-const StyledDivider = styled(Divider)`
-  margin-left: 1rem;
-`;
-
-const StyledCustomButton = styled(CustomButton)`
-  padding: 0.75rem;
-`;
-
-const PaddedStyledCustomButton = styled(StyledCustomButton)`
-  margin-bottom: 1.5rem;
-`;
+import { MainMenu } from './MainMenu';
 
 const CustomIconButton = styled(IconButton)`
   padding: 0;
@@ -92,9 +63,34 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({
   actionText,
   handleCallback,
 }) => {
+  const MenuObject = React.useMemo(
+    () => ({
+      userBalance,
+      stablecoinSymbol,
+      address,
+      profileLinks,
+      handleSecondaryAction,
+      handleProfileAction,
+      secondaryActionText,
+      actionText,
+      handleCallback,
+    }),
+    [
+      userBalance,
+      stablecoinSymbol,
+      address,
+      profileLinks,
+      handleSecondaryAction,
+      handleProfileAction,
+      secondaryActionText,
+      actionText,
+      handleCallback,
+    ],
+  );
+
   const [openMenu, setOpenMenu] = React.useState(false);
   const theme = useTheme();
-  const { t } = useTranslation(['common']);
+
   const buttonStyles = { marginLeft: 'inherit', width: 'max-content' };
   const handleOpen = React.useCallback(() => {
     setOpenMenu(true);
@@ -104,13 +100,6 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({
     setOpenMenu(false);
     if (handleClick) handleClick();
   }, [handleClick]);
-  const balance =
-    typeof userBalance === 'undefined' ? (
-      <Loading size="xs" hasContainer={false} />
-    ) : (
-      `${roundToTwo(userBalance ?? 0)} ${stablecoinSymbol}`
-    );
-  const customAddressStyle = { width: 'auto' };
 
   return (
     <>
@@ -155,111 +144,7 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({
           )}
           {walletAvailable && (
             <StyledGridAvatar item>
-              <StyledGrid container direction="column" spacing={2} theme={theme}>
-                <Grid item className="header-container" marginBottom="2.5rem">
-                  <Identicon alt={address} seed={address} type="tzKtCat" iconSize="xl" />
-                  {address && (
-                    <Address
-                      address={address}
-                      trim
-                      trimSize="medium"
-                      customStyle={customAddressStyle}
-                    />
-                  )}
-                </Grid>
-                <Grid item>
-                  <Typography
-                    component="div"
-                    size="h3"
-                    color={theme.palette.primary.main}
-                    paddingX="0.5rem"
-                  >
-                    {t('positionSummary')}
-                  </Typography>
-                  <Grid container display="flex" marginTop="1.5rem" justifyContent="space-between">
-                    <Typography paddingX="0.5rem" color={theme.palette.text.secondary}>
-                      {t('availableBalance')}
-                    </Typography>
-                    <Typography
-                      component="div"
-                      size="subtitle2"
-                      paddingX="0.5rem"
-                      color={theme.palette.grey[900]}
-                    >
-                      {balance}
-                    </Typography>
-                  </Grid>
-                  <Grid container display="flex" marginTop="1.5rem" justifyContent="space-between">
-                    <Typography paddingX="0.5rem" color={theme.palette.text.secondary}>
-                      {t('openPositions')}
-                    </Typography>
-                    <Typography
-                      component="div"
-                      size="subtitle2"
-                      paddingX="0.5rem"
-                      color={theme.palette.grey[900]}
-                    >
-                      {balance}
-                    </Typography>
-                  </Grid>
-                </Grid>
-                {profileLinks && profileLinks.length > 0 && (
-                  <Grid item marginTop="1.5rem">
-                    {profileLinks.map((link, index) => (
-                      <React.Fragment key={`${link.label}-${index}`}>
-                        <Grid
-                          item
-                          display="flex"
-                          alignItems="center"
-                          justifyContent="center"
-                          marginBottom="1.5rem"
-                        >
-                          <StyledCustomButton
-                            fullWidth
-                            lowercase
-                            variant="contained"
-                            backgroundVariant="secondary"
-                            label={link.label}
-                            onClick={handleProfileAction}
-                          />
-                        </Grid>
-                      </React.Fragment>
-                    ))}
-                  </Grid>
-                )}
-                <StyledDivider />
-                <SettingDialog />
-                <StyledDivider />
-                <Grid
-                  item
-                  container
-                  paddingTop="0.5rem"
-                  justifyContent="center"
-                  flexDirection="column"
-                  alignContent="center"
-                  alignItems="center"
-                >
-                  {secondaryActionText && (
-                    <PaddedStyledCustomButton
-                      lowercase
-                      fullWidth
-                      variant="contained"
-                      backgroundVariant="secondary"
-                      label={secondaryActionText}
-                      onClick={handleSecondaryAction}
-                    />
-                  )}
-                  <StyledCustomButton
-                    lowercase
-                    fullWidth
-                    label={actionText}
-                    variant="contained"
-                    backgroundVariant="primary"
-                    size="medium"
-                    onClick={handleCallback}
-                  />
-                </Grid>
-              </StyledGrid>
+              <MainMenu {...MenuObject} />
             </StyledGridAvatar>
           )}
         </FullSizeDrawer>
--- src/design-system/molecules/ProfilePopover/ProfilePopover.tsx
+++ src/design-system/molecules/ProfilePopover/ProfilePopover.tsx
@@ -1,35 +1,8 @@
 import * as React from 'react';
-import { Popover, Divider, Grid, useTheme } from '@material-ui/core';
-import styled from '@emotion/styled';
-import { useTranslation } from 'react-i18next';
-import { Identicon } from '../../atoms/Identicon';
-import { Typography } from '../../atoms/Typography';
-import { Address } from '../../atoms/Address/Address';
-import { CustomButton } from '../../atoms/Button';
+import { Popover } from '@material-ui/core';
 import { Links } from '../../../interfaces';
-import { roundToTwo } from '../../../utils/math';
-import { Loading } from '../../atoms/Loading';
-import { SettingDialog } from '../SettingDialog';
+import { MainMenu } from '../Header/MainMenu';
 
-const StyledGrid = styled(Grid)`
-  padding: 1rem;
-  .settings {
-    padding-top: 0;
-  }
-  .header-container {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-  }
-`;
-
-const StyledDivider = styled(Divider)`
-  margin-left: 1rem;
-`;
-
-const StyledCustomButton = styled(CustomButton)`
-  padding: 0.75rem;
-`;
 export interface ProfilePopoverProps {
   address: string;
   network: string;
@@ -58,16 +31,21 @@ export const ProfilePopoverComponent: React.FC<ProfilePopoverProps> = ({
   handleCallback,
   actionText,
 }: ProfilePopoverProps) => {
-  const { t } = useTranslation(['common']);
-  const theme = useTheme();
   const id = isOpen ? 'profile-popover' : undefined;
-  const customAddressStyle = { width: 'auto' };
-  const balance =
-    typeof userBalance === 'undefined' ? (
-      <Loading size="xs" hasContainer={false} />
-    ) : (
-      `${roundToTwo(userBalance ?? 0)} ${stablecoinSymbol}`
-    );
+
+  const MenuObject = React.useMemo(
+    () => ({
+      userBalance,
+      stablecoinSymbol,
+      address,
+      profileLinks: links,
+      handleSecondaryAction: handleCallback,
+      handleProfileAction: handleAction,
+      actionText,
+      handleCallback,
+    }),
+    [userBalance, stablecoinSymbol, address, links, handleCallback, handleAction, actionText],
+  );
 
   return (
     <Popover
@@ -84,96 +62,7 @@ export const ProfilePopoverComponent: React.FC<ProfilePopoverProps> = ({
         horizontal: 'right',
       }}
     >
-      <StyledGrid container direction="column" spacing={2} theme={theme}>
-        <Grid item className="header-container" marginBottom="2.5rem">
-          <Identicon alt={address} seed={address} type="tzKtCat" iconSize="xl" />
-          {address && (
-            <Address address={address} trim trimSize="medium" customStyle={customAddressStyle} />
-          )}
-        </Grid>
-        <Grid item>
-          <Typography
-            component="div"
-            size="h3"
-            color={theme.palette.primary.main}
-            paddingX="0.5rem"
-          >
-            {t('positionSummary')}
-          </Typography>
-          <Grid container display="flex" marginTop="1.5rem" justifyContent="space-between">
-            <Typography paddingX="0.5rem" color={theme.palette.text.secondary}>
-              {t('availableBalance')}
-            </Typography>
-            <Typography
-              component="div"
-              size="subtitle2"
-              paddingX="0.5rem"
-              color={theme.palette.grey[900]}
-            >
-              {balance}
-            </Typography>
-          </Grid>
-          <Grid container display="flex" marginTop="1.5rem" justifyContent="space-between">
-            <Typography paddingX="0.5rem" color={theme.palette.text.secondary}>
-              {t('openPositions')}
-            </Typography>
-            <Typography
-              component="div"
-              size="subtitle2"
-              paddingX="0.5rem"
-              color={theme.palette.grey[900]}
-            >
-              {balance}
-            </Typography>
-          </Grid>
-        </Grid>
-        {links && links.length > 0 && (
-          <Grid item marginTop="1.5rem">
-            {links.map((link, index) => (
-              <React.Fragment key={`${link.label}-${index}`}>
-                <Grid
-                  item
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                  marginBottom="1.5rem"
-                >
-                  <StyledCustomButton
-                    fullWidth
-                    lowercase
-                    variant="contained"
-                    backgroundVariant="secondary"
-                    label={link.label}
-                    onClick={handleAction}
-                  />
-                </Grid>
-              </React.Fragment>
-            ))}
-          </Grid>
-        )}
-        <StyledDivider />
-        <SettingDialog />
-        <StyledDivider />
-        <Grid
-          item
-          container
-          paddingTop="0.5rem"
-          justifyContent="center"
-          flexDirection="column"
-          alignContent="center"
-          alignItems="center"
-        >
-          <StyledCustomButton
-            lowercase
-            fullWidth
-            label={actionText}
-            variant="contained"
-            backgroundVariant="primary"
-            size="medium"
-            onClick={handleCallback}
-          />
-        </Grid>
-      </StyledGrid>
+      <MainMenu {...MenuObject} />
     </Popover>
   );
 };
