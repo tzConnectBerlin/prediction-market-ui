2e93054b786ddad62dd87c72912fd95480f6f10d
shekhar-shubhendu <shubh.shekhar1991@gmail.com>
2021-10-01 09:50:25 +0200

    fix: declare prefix as constant

--- src/wallet/utils.ts
+++ src/wallet/utils.ts
@@ -2,6 +2,9 @@ import secp256k1 from 'secp256k1';
 import sodium from 'libsodium-wrappers';
 import { hex2buf, b58cencode } from '@taquito/utils';
 
+const SECRET_KEY_PREFIX = new Uint8Array([17, 162, 224, 201]);
+const PUBLIC_KEY_HASH_PREFIX = new Uint8Array([6, 161, 161]);
+
 interface WalletInfo {
   secretKey: string;
   address: string;
@@ -9,11 +12,11 @@ interface WalletInfo {
 
 const publicKeyHash = async (key: any) => {
   await sodium.ready;
-  return b58cencode(sodium.crypto_generichash(20, key), new Uint8Array([6, 161, 161]));
+  return b58cencode(sodium.crypto_generichash(20, key), PUBLIC_KEY_HASH_PREFIX);
 };
 
 const getSecretKey = (key: any) => {
-  return b58cencode(key, new Uint8Array([17, 162, 224, 201]));
+  return b58cencode(key, SECRET_KEY_PREFIX);
 };
 
 export const getAddressAndSecretKey = async (key: any): Promise<WalletInfo> => {
