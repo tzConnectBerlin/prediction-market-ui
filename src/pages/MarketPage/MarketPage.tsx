aba54863777e4a9c6792b8adb7726ead77b2144b
Shohreh Modarrei <sh.modarresy@gmail.com>
2021-10-21 11:38:47 +0200

    feat(i18n): change resolution to outcome (#565)

--- src/pages/MarketPage/MarketPage.tsx
+++ src/pages/MarketPage/MarketPage.tsx
@@ -586,7 +586,7 @@ export const MarketPageComponent: React.FC<MarketPageProps> = ({ market }) => {
     if (market?.winningPrediction && marketHeader.stats) {
       marketHeader.stats.push(
         {
-          label: t('resolution'),
+          label: t('outcome'),
           value: market.winningPrediction.toUpperCase(),
         },
         {
